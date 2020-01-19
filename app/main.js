ce9bf934d20bb4c03619f8e0ff42e0858a24fa7f
RD <rory.c.dickinson@gmail.com>
2020-01-19 15:37:04 -0600

    update icon display logic

--- app/main.js
+++ app/main.js
@@ -1,31 +1,46 @@
 
 const path = require('path');
-const { app, Menu, Tray } = require('electron');
+const { app, Menu, Tray, systemPreferences } = require('electron');
 
+const clippings = [];
 // eventually stores the Tray instance
 let tray = null;
 
-app.on('ready', () => {
+// check the platform and returns appropriate icon
+const getIcon = () => {
+    if (process.platform === 'win32') return 'icon-light@2x.ico';
+    if (systemPreferences.isDarkMode()) return 'icon-light.png';
+    return 'icon-dark.png';
+};
 
+app.on('ready', () => {
     // hides the dock icon if running on MacOS
     if (app.dock) app.dock.hide();
 
-    tray = new Tray(path.join(__dirname, '/Icon.png'));
+    tray = new Tray(path.join(__dirname, getIcon()));
 
     // if Windows, register click event listener to open menu
     if (process.platform === 'win32') {
         tray.on('click', tray.popUpContextMenu);
     }
+    updateMenu();
+    tray.setToolTip('Clipmaster');
+});
 
+const updateMenu = () => {
     // build the menu
     const menu = Menu.buildFromTemplate([
+        {
+            label: 'Create New Clipping',
+            click() { null; }
+        },
+        { type: 'separator' },
+        ...clippings.map( (clipping, index) => ({ label: clipping })),
+        { type: 'separator' },
         {
             label: 'Quit',
             click() { app.quit() }
         }
     ]);
-
-    tray.setToolTip('Clipmaster');
     tray.setContextMenu(menu);
-});
-
+};
